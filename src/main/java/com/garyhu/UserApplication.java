270b260d4752d877bbad463bf280710695c737bd
huweiwei <huweiwei@superengine.com.cn>
2018-11-09 21:00:25 +0800

    添加微服务

--- src/main/java/com/garyhu/UserApplication.java
+++ src/main/java/com/garyhu/UserApplication.java
@@ -2,6 +2,7 @@ package com.garyhu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author: garyhu
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @decription:
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserApplication {
 
     public static void main(String[] args)throws Exception {
